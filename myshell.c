5caa88b13845a5e18b2d5b86b0ad3395ec0633d0
RalpiRekt <elrincondeadriancm@gmail.com>
2026-01-01 17:37:44 +0100

    Objetivo 2 completado: Redirecciones de entrada y salida

--- myshell.c
+++ myshell.c
@@ -3,6 +3,7 @@
 #include <unistd.h>      // Para fork(), execvp()
 #include <sys/types.h>   // Para pid_t
 #include <sys/wait.h>    // Para wait()
+#include <fcntl.h>       // ¡IMPORTANTE! Para open(), O_CREAT, O_RDONLY
 #include "parser.h"      // Librería del profesor
 
 int main(void) {
@@ -11,42 +12,62 @@ int main(void) {
     pid_t pid;
     int status;
 
-    printf("msh> "); // El prompt
+    printf("msh> "); 
 
-    // Bucle infinito: lee línea a línea del teclado
     while (fgets(buf, 1024, stdin)) {
         
-        // El parser trocea la línea por nosotros
         line = tokenize(buf);
 
-        // Si la línea está vacía, pedimos otra vez
         if (line == NULL || line->ncommands == 0) {
             printf("msh> ");
             continue;
         }
 
-        // --- EJECUCIÓN DEL COMANDO ---
-        pid = fork(); // 1. Creamos proceso hijo
+        // --- EJECUCIÓN ---
+        pid = fork(); 
 
         if (pid < 0) { 
             fprintf(stderr, "Error al hacer fork\n");
             exit(1);
         }
         else if (pid == 0) { 
-            // 2. CÓDIGO DEL HIJO
-            // Ejecuta el comando (ej: ls, whoami)
+            // --- CÓDIGO DEL HIJO ---
+
+            // 1. Redirección de Entrada (<)
+            if (line->redirect_input != NULL) {
+                int fd = open(line->redirect_input, O_RDONLY);
+                if (fd < 0) {
+                    fprintf(stderr, "%s: Error. No se puede abrir entrada\n", line->redirect_input);
+                    exit(1);
+                }
+                dup2(fd, 0); 
+                close(fd);
+            }
+
+            // 2. Redirección de Salida (>)
+            if (line->redirect_output != NULL) {
+                int fd = open(line->redirect_output, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+                if (fd < 0) {
+                    fprintf(stderr, "%s: Error. No se puede abrir salida\n", line->redirect_output);
+                    exit(1);
+                }
+                dup2(fd, 1); 
+                close(fd);
+            }
+
+            // 3. Ejecutar el mandato
             execvp(line->commands[0].filename, line->commands[0].argv);
             
-            // Si llega aquí, es que falló (comando no existe)
+            // Si llega aquí, es que falló
             fprintf(stderr, "%s: No se encuentra el mandato\n", line->commands[0].filename);
             exit(1);
         }
         else {
-            // 3. CÓDIGO DEL PADRE
-            wait(&status); // Espera a que el hijo termine
+            // --- CÓDIGO DEL PADRE ---
+            wait(&status); 
         }
 
-        printf("msh> "); // Vuelve a sacar el prompt
+        printf("msh> "); 
     }
     return 0;
 }
